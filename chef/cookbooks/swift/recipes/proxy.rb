8d8120111849f7aefb9c48bd4e4367e7c7cc01bb
Sascha Peilicke <saschpe@gmx.de>
2013-07-16 17:47:47 +0200

    Port to Havana
    Don't explicitly set "workers", the default is using #cpus. Drop
    auth-server.conf.erb, functionality dropped upstream. A copyright in
    config files is odd. Remove "setup_console_handler", doesn't exist
    anymore.
    
    Updated PFS dependencies.

--- chef/cookbooks/swift/recipes/proxy.rb
+++ chef/cookbooks/swift/recipes/proxy.rb
@@ -1,5 +1,6 @@
 #
 # Copyright 2011, Dell
+# Copyright 2013, SUSE
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Author: andi abes
+# Author: andi abes, sascha peilicke
 #
 
 include_recipe 'utils'
